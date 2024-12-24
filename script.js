112b4b0198ddded5271a8a4954701ef0e2ce04b4
deepraj-07 <deepraj5915@gmail.com>
2024-12-24 20:55:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("HEllo,World");
+console.log("Hello Guys");
+
+let a="Hello";//String
+let b=3;      //Int
+let c= true;  //Boolean
+let d;        //Undefined
+let r= null;  //Null
+console.log(typeof a,typeof b, typeof c, typeof d, typeof r);
